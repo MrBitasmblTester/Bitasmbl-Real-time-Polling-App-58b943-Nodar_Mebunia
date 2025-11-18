da9518fa646028c446c8f3b8577332d3cae18fbd
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-18 17:25:08 +0400

    Add App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <h1>Real-time Polls</h1>
+    <poll-editor />
+    <poll-list />
+  </div>
+</template>
+<script>
+import PollList from './components/PollList.vue';
+import PollEditor from './components/PollEditor.vue';
+export default { components:{ PollList, PollEditor } }
+</script>
