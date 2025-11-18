e12b8819a2bf4116cfb2bc054f756746aec09b62
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-18 17:25:10 +0400

    Add PollChart.vue

---
+++ frontend/src/components/PollChart.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg :width="200" :height="(Object.keys(data).length*24+10)">
+    <g v-for="(v,k,i) in data" :key="k">
+      <rect :x="0" :y="i*24" :width="Math.max(1,v*10)" height="18" fill="#4f46e5" />
+      <text :x="Math.max(4,v*10)+6" :y="i*24+14">{{k}} ({{v}})</text>
+    </g>
+  </svg>
+</template>
+<script>
+export default { props:['data'] }
+</script>
